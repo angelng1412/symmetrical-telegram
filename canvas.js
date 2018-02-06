b492c7886a93d9f5ad3fdb3c8defc6a2dbfd8363
angelng1412 <ang7@stuy.edu>
2018-02-05 20:56:30 -0500

    connect the dots

--- canvas.js
+++ canvas.js
@@ -1,16 +1,18 @@
 var canvas = document.getElementById("slate");
 var context = canvas.getContext("2d");
-context.fillStyle = "#ff0000"; 
+context.fillStyle = "#ff0000";
+context.strokeStyle = "#ff0000"; 
 
 var reset = document.getElementById("reset");
 var toggle = document.getElementById("toggle");
 
-var shape = 0; 
-
+var shape = 0; //default circle, 1 for square 
+var count = 0; //number of circles in canvas
 
 var resetCanvas = function(e){
     context.beginPath(); 
     context.clearRect(0, 0, canvas.width, canvas.height);
+    count = 0; 
 }
 
 var toggleShape = function(e){
@@ -24,15 +26,19 @@ var toggleShape = function(e){
 
 var drawShape = function(e){
     if (shape == 0){
+	if (count > 0){
+	    context.lineTo(e.offsetX, e.offsetY);
+	    context.stroke();
+	}
 	context.beginPath();
-	context.arc(e.clientX, e.clientY, 10, 0, 2*Math.PI);
+	context.arc(e.offsetX, e.offsetY, 10, 0, 2*Math.PI);
 	context.stroke();
 	context.fill();
+	context.moveTo(e.offsetX, e.offsetY); 
+	count += 1;
     }
     else{
-	context.beginPath();
-	context.rect(e.clientX, e.clientY, 20, 20);
-	context.stroke();
+	context.rect(e.offsetX, e.offsetY, 20, 20);
 	context.fill();
     }
 }
